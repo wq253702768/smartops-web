5d0e91a9f310cc702595ceb8e1817c0db5d8fd50
wq253702768 <qi.wen@tradeshift.com>
2025-06-21 09:33:36 +0800

    Fix missing vue-query css import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import { pinia } from './stores'
-import '@tanstack/vue-query/dist/vue-query.css'
 import './styles/tailwind.css'
 
 createApp(App).use(router).use(pinia).mount('#app')
